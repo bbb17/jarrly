afd57867e59fcf132a6bd7cf87ee8b3839fb5395
Bhas <bnalabot@gmail.com>
2013-05-10 20:15:26 -0400

    first comm

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jarrly::Application.config.secret_token = '0e6a3b44e15a30244ce04f90403b2e01492915ac6b709bd13f526b05b1689d6f7bedc6060e8483b738afdbb8550b1d21e8ba3dbfcccf8721e5de4f3cb1733009'
